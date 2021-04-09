b9a5a8178154d0c0dcdbc2e703c90f2846d852f4
sychova <anastasiya.sychova@gmail.com>
2021-04-09 03:58:45 +0300

    comment

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const app = express();
-const myfirstmodule = require("./myfirstmodule");
+const mssql = require("mssql/msnodesqlv8");
+
+app.get("/", function(req, res) {
+    require("msnodesqlv8");
+
+    const config = {
+        server: "(LocalDB)\\MSSQLLocalDB",
+        database: "Homework2",
+        port: "1433",
+        driver: "msnodesqlv8",
+        options: {
+            trustedConnection: true
+        }
+    };
+
+    console.log("Starting");
+
+    const pool = new mssql.ConnectionPool(config);
+    pool.connect().then(() => {
+        pool.request().query("SELECT * FROM Users", function(err, recordset) {
+            if (err) res.send(err);
+            else {
+                res.send(recordset);
+            }
+        });
+    });
+    mssql.close();
+});
 
 // Showing pages
-app.get("/", function(req, res) {
-    res.send("Hello world!");
-})
+// app.get("/", function(req, res) {
+//     res.send("Hello world!");
+// })
 
-app.get("/siski", function(req, res) {
-    res.sendFile(__dirname + "/indexx.html");
-})
+// app.get("/siski", function(req, res) {
+//     res.sendFile(__dirname + "/indexx.html");
+// })
 
-myfirstmodule.log("pizdec");
+// myfirstmodule.log("pizdec");
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
