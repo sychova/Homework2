785ebd21e4405d288531c4a2fe0d20529d6b6473
sychova <anastasiya.sychova@gmail.com>
2021-11-25 23:25:07 +0300

    simplified the sortBy method

--- userServiceSQLHelper.js
+++ userServiceSQLHelper.js
@@ -1,19 +1,5 @@
 function sortBy(sortBy) {
-    if (sortBy === "FirstName") {
-        return "FirstName";
-    } else if (sortBy === "LastName") {
-        return "LastName";
-    } else if (sortBy === "Age") {
-        return "Age";
-    } else if (sortBy === "Phone") {
-        return "Phone";
-    } else if (sortBy === "Email") {
-        return "Email";
-    } else if (sortBy === "Gender") {
-        return "Gender";
-    } else {
-        return "UserID"
-    }
+    return sortBy || "UserID"
 };
 
 function sortDirection(sortDirection) {
