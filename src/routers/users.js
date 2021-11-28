b59a906fa8b201a77d3197d066e71727975f1d5e
sychova <anastasiya.sychova@gmail.com>
2021-11-29 01:57:50 +0300

    server files cleanup, favicon added

--- src/routers/users.js
+++ src/routers/users.js
@@ -6,7 +6,7 @@ const {
     editUser,
     updateUser,
     createUser
-} = require("../userService")
+} = require("../utils/userService")
 
 router.get("/", (req, res) => {
     res.render("index.pug")
